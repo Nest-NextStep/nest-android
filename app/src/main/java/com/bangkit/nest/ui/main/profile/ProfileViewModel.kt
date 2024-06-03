f242368d2bd0714245fa3c374271d6bfd00b2a62
devinahana <112392609+devinahana@users.noreply.github.com>
2024-06-03 20:52:55 +0700

    profile (#8)
    * feat: create login viewmodel and fragment
    
    * style: add corner radius, text style, and button style
    
    * feat: add register fragment
    
    * feat: login, register functionality
    
    * fix merge conflict
    
    * feat: ui profile page
    
    * style: change navigation bar color to white
    
    * style: add toggle navbar visibility method

--- app/src/main/java/com/bangkit/nest/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/bangkit/nest/ui/main/profile/ProfileViewModel.kt
@@ -3,10 +3,17 @@ package com.bangkit.nest.ui.main.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bangkit.nest.data.repository.UserPrefRepository
+import kotlinx.coroutines.launch
 
-class ProfileViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+class ProfileViewModel(
+    private val userPrefRepository: UserPrefRepository,
+) : ViewModel() {
+
+    fun logout() {
+        viewModelScope.launch {
+            userPrefRepository.logout()
+        }
     }
-    val text: LiveData<String> = _text
 }
