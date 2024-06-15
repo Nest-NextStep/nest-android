8be484c395ec4bfe1fe8a9d6d7d492159bbcb874
Devina <devina.hana@ui.ac.id>
2024-06-15 21:53:54 +0700

    style: fix layout

--- app/src/main/java/com/bangkit/nest/ui/main/catalog/CatalogViewModel.kt
+++ app/src/main/java/com/bangkit/nest/ui/main/catalog/CatalogViewModel.kt
@@ -22,8 +22,35 @@ class CatalogViewModel(
 
     private var isDataLoaded = false
 
+    init {
+        getAllMajor()
+    }
+
     fun reloadAllMajor() {
-        isDataLoaded = false
+        majorRepository.getAllMajor().observeForever { result ->
+            when (result) {
+                is Result.Loading -> {
+
+                }
+                is Result.Success -> {
+                    if (!isDataSame(result.data.majorRecommended, result.data.majorsAll)) {
+                        _majorRecommended.value = result.data.majorRecommended
+                        _majorsAll.value = result.data.majorsAll
+                        _state.value = Result.Success(Unit)
+                    }
+                    isDataLoaded = true
+                }
+                is Result.Error -> {
+                    _state.value = Result.Error(result.error)
+                }
+            }
+        }
+    }
+
+    private fun isDataSame(newMajorRecommended: List<MajorItem>, newMajorAll: List<MajorItem>): Boolean {
+        val currentRecommendedSet = _majorRecommended.value?.toSet()
+        val currentAllSet = _majorsAll.value?.toSet()
+        return newMajorRecommended.toSet() == currentRecommendedSet && newMajorAll.toSet() == currentAllSet
     }
 
     fun getAllMajor() {
